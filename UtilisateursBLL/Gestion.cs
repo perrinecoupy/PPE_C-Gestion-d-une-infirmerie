ac248a8ffa2ca3f8caf0527f132a771ad2dd94af
Arthur <valentin.arthur1000@gmail.com>
2021-11-18 16:44:56 +0100

    Ajout différents fichiers + méthodes de connexion + formulaire de connexion

---
+++ UtilisateursBLL/Gestion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using UtilisateursDAL;
+using UtilisateursBO;
+
+namespace UtilisateursBLL
+{
+    public class Gestion
+    {
+        private static Gestion uneGestionUtilisateurs; // objet BLL
+
+        // Accesseur en lecture
+        public static Gestion GetGestionUtilisateurs()
+        {
+            if (uneGestionUtilisateurs == null)
+            {
+                uneGestionUtilisateurs = new Gestion();
+            }
+
+            return uneGestionUtilisateurs;
+        }
+
+        // Définit la chaîne de connexion grâce à la méthode SetchaineConnexion de la DAL
+        public static void SetchaineConnexion(ConnectionStringSettings chset)
+        {
+            string chaine = chset.ConnectionString;
+            ConnexionBD.GetConnexionBD().SetchaineConnexion(chaine);
+        }
+
+        // Méthode qui retourne vrai si les informations de connexion sont les bonnes
+        public static bool EstConnecte(string login, string mdp)
+        {
+            return GestionDAO.EstConnecte(login, mdp);
+        }
+    }
+}
