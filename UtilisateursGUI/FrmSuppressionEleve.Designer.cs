5bdd4218f17a0fbae3dfb0989dd5f1cca525fbd5
Perrine <perrinecoupy@gmail.com>
2021-11-18 16:51:40 +0100

    Suppression d'un élève : mise à jour de la présentation

--- UtilisateursGUI/FrmSuppressionEleve.Designer.cs
+++ UtilisateursGUI/FrmSuppressionEleve.Designer.cs
@@ -31,57 +31,73 @@ namespace UtilisateursGUI
         {
             this.lblSuppressionEleve = new System.Windows.Forms.Label();
             this.bnbOuiSuppressionEleve = new System.Windows.Forms.Button();
-            this.bnbNonSuppressionEleve = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblSuppressionEleve
             // 
             this.lblSuppressionEleve.AutoSize = true;
-            this.lblSuppressionEleve.Location = new System.Drawing.Point(47, 144);
+            this.lblSuppressionEleve.Location = new System.Drawing.Point(70, 246);
+            this.lblSuppressionEleve.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSuppressionEleve.Name = "lblSuppressionEleve";
-            this.lblSuppressionEleve.Size = new System.Drawing.Size(169, 13);
+            this.lblSuppressionEleve.Size = new System.Drawing.Size(250, 20);
             this.lblSuppressionEleve.TabIndex = 24;
             this.lblSuppressionEleve.Text = "Voulez-vous supprimer cet élève ?";
             // 
             // bnbOuiSuppressionEleve
             // 
-            this.bnbOuiSuppressionEleve.Location = new System.Drawing.Point(63, 193);
+            this.bnbOuiSuppressionEleve.Location = new System.Drawing.Point(74, 292);
+            this.bnbOuiSuppressionEleve.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bnbOuiSuppressionEleve.Name = "bnbOuiSuppressionEleve";
-            this.bnbOuiSuppressionEleve.Size = new System.Drawing.Size(104, 41);
+            this.bnbOuiSuppressionEleve.Size = new System.Drawing.Size(134, 34);
             this.bnbOuiSuppressionEleve.TabIndex = 25;
             this.bnbOuiSuppressionEleve.Text = "Oui";
             this.bnbOuiSuppressionEleve.UseVisualStyleBackColor = true;
             // 
-            // bnbNonSuppressionEleve
-            // 
-            this.bnbNonSuppressionEleve.Location = new System.Drawing.Point(375, 193);
-            this.bnbNonSuppressionEleve.Name = "bnbNonSuppressionEleve";
-            this.bnbNonSuppressionEleve.Size = new System.Drawing.Size(104, 41);
-            this.bnbNonSuppressionEleve.TabIndex = 26;
-            this.bnbNonSuppressionEleve.Text = "Non";
-            this.bnbNonSuppressionEleve.UseVisualStyleBackColor = true;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::UtilisateursGUI.Properties.Resources.logoinfirmiere;
-            this.pictureBox1.Location = new System.Drawing.Point(50, 43);
+            this.pictureBox1.Location = new System.Drawing.Point(74, 48);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(117, 47);
+            this.pictureBox1.Size = new System.Drawing.Size(245, 110);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabIndex = 27;
             this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 292);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 34);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Non";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "SUPPRESSION D\'UN ELEVE";
             // 
             // FrmSuppressionEleve
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 450);
-            this.Controls.Add(this.bnbNonSuppressionEleve);
+            this.ClientSize = new System.Drawing.Size(749, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.bnbOuiSuppressionEleve);
             this.Controls.Add(this.lblSuppressionEleve);
-            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmSuppressionEleve";
             this.Text = "SuppressionEleve";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -91,10 +107,10 @@ namespace UtilisateursGUI
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblSuppressionEleve;
         private System.Windows.Forms.Button bnbOuiSuppressionEleve;
-        private System.Windows.Forms.Button bnbNonSuppressionEleve;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
