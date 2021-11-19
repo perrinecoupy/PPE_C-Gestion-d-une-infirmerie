d1b10c28f17afae1eca7d9e116891245d229809c
Perrine <perrinecoupy@gmail.com>
2021-11-19 16:57:55 +0100

    UtilisateursBO Classe : attributs, constructeur, getter, setter

--- UtilisateursBO/Eleve.cs
+++ UtilisateursBO/Eleve.cs
@@ -8,5 +8,46 @@ namespace UtilisateursBO
 {
     public class Eleve
     {
+        int id;
+        string Nom;
+        string Prenom;
+        int Age;
+        char Sexe;
+        DateTime DateNaissance;
+        string Sante;
+        string NumTelEleve;
+        string NumTelParent;
+        string TiersTemps;
+        string Commentaire;
+        int Classe;
+
+        public Eleve(int id, string nom, string prenom, int age, char sexe, DateTime dateNaissance, string sante, string numTelEleve, string numTelParent, string tiersTemps, string commentaire, int classe)
+        {
+            this.Id = id;
+            Nom1 = nom;
+            Prenom1 = prenom;
+            Age1 = age;
+            Sexe1 = sexe;
+            DateNaissance1 = dateNaissance;
+            Sante1 = sante;
+            NumTelEleve1 = numTelEleve;
+            NumTelParent1 = numTelParent;
+            TiersTemps1 = tiersTemps;
+            Commentaire1 = commentaire;
+            Classe1 = classe;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom1 { get => Nom; set => Nom = value; }
+        public string Prenom1 { get => Prenom; set => Prenom = value; }
+        public int Age1 { get => Age; set => Age = value; }
+        public char Sexe1 { get => Sexe; set => Sexe = value; }
+        public DateTime DateNaissance1 { get => DateNaissance; set => DateNaissance = value; }
+        public string Sante1 { get => Sante; set => Sante = value; }
+        public string NumTelEleve1 { get => NumTelEleve; set => NumTelEleve = value; }
+        public string NumTelParent1 { get => NumTelParent; set => NumTelParent = value; }
+        public string TiersTemps1 { get => TiersTemps; set => TiersTemps = value; }
+        public string Commentaire1 { get => Commentaire; set => Commentaire = value; }
+        public int Classe1 { get => Classe; set => Classe = value; }
     }
 }
--- UtilisateursDAL/GestionDAO.cs
+++ UtilisateursDAL/GestionDAO.cs
@@ -53,4 +53,34 @@ namespace UtilisateursDAL
             return false;
         }
     }
+
+    // Méthode qui ajoute un adhérent dans la base de données
+    public static void AjoutEleve (Eleve eleve)
+    {
+        // Connexion à la BD
+        SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+
+        // Requette sql
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = maConnexion;
+        cmd.CommandText = "INSERT INTO Eleve (nom_eleve, prenom_eleve, age_eleve, sexe_eleve, date_de_naissance_eleve, sante_eleve, numero_telephone_eleve, numero_telephone_parent_eleve, tiers_temps_eleve, commentaires_sante_libre_eleve, #id_classe_eleve) VALUES (@Nom_eleve, @Prenom_eleve, @Age_eleve, @Sexe_eleve, @Date_de_naissance_eleve, @Sante_eleve, @Numero_telephone_eleve, @Numero_telephone_parent_eleve, @Tiers_temps_eleve, @Commentaires_sante_libre_eleve, @Id_classe_eleve)";
+
+        // Ajout des paramètres
+        cmd.Parameters.AddWithValue("@Nom_eleve", eleve.Nom);
+        cmd.Parameters.AddWithValue("@Prenom_eleve", eleve.Prenom);
+        cmd.Parameters.AddWithValue("@Age_eleve", eleve.Age);
+        cmd.Parameters.AddWithValue("@Sexe_eleve", eleve.Sexe);
+        cmd.Parameters.AddWithValue("@Date_de_naissance_eleve", eleve.DateNaissance);
+        cmd.Parameters.AddWithValue("@Sante_eleve", eleve.Sante);
+        cmd.Parameters.AddWithValue("@Numero_telephone_eleve", eleve.NumeroTelEleve);
+        cmd.Parameters.AddWithValue("@Numero_telephine_parent_eleve", eleve.NumTelParent);
+        cmd.Parameters.AddWithValue("@Tiers_temps_eleve", eleve.TiersTemps);
+        cmd.Parameters.AddWithValue("@Commentaires_sante_libre_eleve", eleve.Commentaire);
+        cmd.Parameters.AddWithValue("@Id_classe_eleve", eleve.Classe);
+
+        // Execution de la requete
+        cmd.ExecuteNonQuery();
+
+        maConnexion.Close();
+    }
 }
